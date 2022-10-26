921b9efaa7080e8e5a35a87930667bc134285227
annashutova <annashut@inbox.ru>
2022-10-26 13:56:03 +0300

    Добавлена вторая функция

--- letters.py
+++ letters.py
@@ -6,3 +6,6 @@ def clone_letters(line: str):
         if line.count(sym) > 1:
             count += 1
     return count
+
+def count_duplicates(s):
+    return len([ch for ch in (set(s.lower())) if s.lower().count(ch) > 1])
