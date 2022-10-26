491d4d9c1333ecd15441d4fc8fab4d63cb2154c7
vecherninanika <nikavecher333@gmail.com>
2022-10-26 14:37:32 +0300

    Добавлен декоратор

--- letters.py
+++ letters.py
@@ -1,3 +1,15 @@
+import time
+def worktime(f1, f2):
+    def f(string):
+        initial_time = time.time()
+        f1(string)
+        first = time.time() - initial_time
+        initial_time = time.time()
+        f2(string)
+        second = time.time() - initial_time
+        print(f1.name if first < second else f2.name)
+    return f
+
 def clone_letters(line: str):
     line = line.lower()
     uniquq_let = set(line)
@@ -9,3 +21,6 @@ def clone_letters(line: str):
 
 def count_duplicates(s):
     return len([ch for ch in (set(s.lower())) if s.lower().count(ch) > 1])
+
+funcion = worktime(count_duplicates, clone_letters)
+funcion('aaaBBnn')
